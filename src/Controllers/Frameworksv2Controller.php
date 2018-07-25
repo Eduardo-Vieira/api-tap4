59d025aea8d35a224e4b2fec74f84fbd8a596f6f
Eduardo Vieira <gceduvieira@gmail.com>
2018-07-24 21:52:11 -0400

    Model com eloquent

--- src/Controllers/Frameworksv2Controller.php
+++ src/Controllers/Frameworksv2Controller.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Controllers;
+
 use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use \App\Models\Frameworks;
 
 class Frameworksv2Controller
 {
@@ -20,9 +22,12 @@ class Frameworksv2Controller
 
     public function index(Request $request, Response $response, $args)
     {
-
-        $data = \App\Models\Frameworks::where('id_frameworks', $args['id_frameworks'])->firstOrFail();
-
+        //Verificar se a url tem agumentos
+        if($args['id_frameworks']){
+            $data = Frameworks::where('id_frameworks', $args['id_frameworks'])->firstOrFail();
+        } else{
+            $data = Frameworks::all();
+        }
         return $response->withJson(["code"=>200,
                                     "data"=>  $data,
                                     "mensage"=>"sucesso!",
